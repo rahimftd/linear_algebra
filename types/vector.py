deb6eb5cd7f86a02dd3349993dd64c2a6a7c0166
Rahim <rahim.dharssi@gmail.com>
2017-03-30 00:01:42 -0400

    Create vector type

---
+++ types/vector.py
@@ -0,0 +1,29 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def plus(self, v):
+        new_vector = []
+        for i in range(len(self.coordinates))
+            new_coord = self.coordinates[i] + v.coordinates[i]
+            new_vector.append(new_coord)
+        return new_vector
+
+    def minus(self, v):
+        
